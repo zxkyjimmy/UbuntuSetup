375893beac1e78e171e0039d690579f20f05b163
Yen-Chi Chen <zxkyjimmy@gmail.com>
2023-09-13 16:49:00 +0800

    create nvidia-container-runtime/config.toml

--- setup.sh
+++ setup.sh
@@ -115,6 +115,9 @@ sudo sed -E 's;# unqualified-search-registries = \["example.com"\];unqualified-s
 step "Install nvidia-container-runtime"
 sudo apt update
 sudo apt install -y nvidia-container-runtime
+# No runtime/config.toml since nvidia-container-toolkit v1.14.1
+# Don't convert to cdi untill podman v4.1.0
+sudo nvidia-ctk config default --output=/etc/nvidia-container-runtime/config.toml
 sudo sed -i 's/^#no-cgroups = false/no-cgroups = true/;' /etc/nvidia-container-runtime/config.toml
 sudo mkdir -p /usr/share/containers/oci/hooks.d
 cat <<EOF | sudo tee /usr/share/containers/oci/hooks.d/oci-nvidia-hook.json
